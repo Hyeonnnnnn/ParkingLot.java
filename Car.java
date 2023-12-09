685982d9f6bb2b7921b60fbeb740d72e9251866e
edgarC <128599934+Hyeonnnnnn@users.noreply.github.com>
2023-12-09 16:07:01 +0800

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private String licensePlate;
+
+    public Car(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getLicensePlate() {
+        return this.licensePlate;
+    }
+}
